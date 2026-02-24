4d815e032305c52e6e37fa90210f9db50f9cf1c0
Stephanie <200150677+Steph7478@users.noreply.github.com>
2026-02-24 10:24:49 -0300

    refactor: metrics to ms

--- src/config/workers/worker.c
+++ src/config/workers/worker.c
@@ -5,6 +5,7 @@
 #include "../router/router.h"
 #include <string.h>
 #include <stdio.h>
+#include <windows.h>
 
 extern ClientQueue clientQueue;
 extern unsigned __int64 get_cpu_cycles();
@@ -12,6 +13,9 @@ extern unsigned __int64 get_cpu_cycles();
 DWORD WINAPI worker_thread(LPVOID param)
 {
     (void)param;
+    LARGE_INTEGER frequency;
+    QueryPerformanceFrequency(&frequency);
+
     while (1)
     {
         unsigned __int64 entry_timestamp;
@@ -21,7 +25,6 @@ DWORD WINAPI worker_thread(LPVOID param)
             continue;
 
         unsigned __int64 exit_queue_timestamp = get_cpu_cycles();
-
         HttpRequest req = {0};
         if (http_parse(c, &req))
         {
@@ -29,12 +32,15 @@ DWORD WINAPI worker_thread(LPVOID param)
             response_init(&res, c);
             router_handle(&req, &res);
         }
-
         unsigned __int64 final_timestamp = get_cpu_cycles();
 
-        printf("[CPU Cycles] Queue Latency: %llu | Processing Time: %llu\n",
-               exit_queue_timestamp - entry_timestamp,
-               final_timestamp - exit_queue_timestamp);
+        unsigned __int64 q_cycles = exit_queue_timestamp - entry_timestamp;
+        unsigned __int64 p_cycles = final_timestamp - exit_queue_timestamp;
+
+        double q_ms = (double)(q_cycles * 1000) / frequency.QuadPart;
+        double p_ms = (double)(p_cycles * 1000) / frequency.QuadPart;
+
+        printf("[Metrics] Queue Latency: %.4f ms | Processing: %.4f ms\n", q_ms, p_ms);
 
         shutdown(c, SD_SEND);
         closesocket(c);
